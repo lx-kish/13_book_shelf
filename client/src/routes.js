e462f263fb94ee816250d3e7a1f9f4e53c3595a6
lx kish <urban.ktulhu@gmail.com>
2018-08-26 23:56:55 +1200

    159 Client: Login screen

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/home';
 import BookView from './components/Books';
+import Login from './containers/Admin/login';
 import Layout from './hoc/layout';
 
 const Routes = () => {
@@ -11,6 +12,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/book/:id" exact component={BookView} />
+                <Route path="/login" exact component={Login}/>
             </Switch>
         </Layout>
     );
