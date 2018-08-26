a6758e4b5751a36a437eb993a5339cc732a574ec
lx kish <urban.ktulhu@gmail.com>
2018-08-26 17:44:33 +1200

    156 Client: Home 2

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,11 +3,18 @@ import axios from 'axios';
 export function getBooks(
     limit = 10,
     start = 0,
-    order = 'asc'
+    order = 'asc',
+    list = ''
 ){
     
     const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
-                    .then(response => response.data)
+                    .then(response => {
+                        if(list) {
+                            return [...list, ...response.data]
+                        } else {
+                            return response.data
+                        }
+                    })
     
     return {
         type: 'GET_BOOKS',
