721a181dbd7e88f9a6e3e0240fe022f4d302b27a
lx kish <urban.ktulhu@gmail.com>
2018-08-28 08:40:47 +1200

    161 Client: Auth HOC 2

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { auth } from '../actions';
 import { connect } from 'react-redux';
 
-export default function(ComposedClass){
+export default function(ComposedClass, reload){
     class AuthenticationCheck extends Component {
 
         state = {
@@ -17,18 +17,23 @@ export default function(ComposedClass){
             this.setState({loading: false})
 
             if(!nextProps.user.login.isAuth) {
-
+                if(reload) {
+                    this.props.history.push('/login');
+                }
             } else {
-                
+                if(reload === false){
+                    this.props.history.push('/user');
+                }
             }
         }
 
         render() {
+
             if(this.state.loading) {
                 return <div className="loader">Loading...</div>
             }
             return (
-                <ComposedClass {...this.props} user=""/>
+                <ComposedClass {...this.props} user={this.props.user}/>
             )
         }
     }
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,17 +4,19 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/home';
 import BookView from './components/Books';
 import Login from './containers/Admin/login';
-import Layout from './hoc/layout';
+import User from './components/Admin/';
 
+import Layout from './hoc/layout';
 import Auth from './hoc/auth';
 
 const Routes = () => {
     return (
         <Layout>
             <Switch>
-                <Route path="/" exact component={Auth(Home)} />
-                <Route path="/book/:id" exact component={BookView} />
-                <Route path="/login" exact component={Login}/>
+                <Route path="/" exact component={Auth(Home, null)} />
+                <Route path="/login" exact component={Auth(Login, false)}/>
+                <Route path="/user" exact component={Auth(User, true)}/>
+                <Route path="/book/:id" exact component={Auth(BookView, null)} />
             </Switch>
         </Layout>
     );
