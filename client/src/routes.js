8635c0ca797ecd32a8af0c62e699b3e852193e62
lx kish <urban.ktulhu@gmail.com>
2018-09-02 13:22:15 +1200

    168 Client: Register users

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,6 +8,7 @@ import User from './components/Admin/';
 import AddReview from './containers/Admin/add';
 import UserPosts from './components/Admin/userPosts';
 import EditReview from './containers/Admin/edit';
+import Register from './containers/Admin/register';
 
 import Layout from './hoc/layout';
 import Auth from './hoc/auth';
@@ -20,6 +21,7 @@ const Routes = () => {
                 <Route path="/login" exact component={Auth(Login, false)}/>
                 <Route path="/user" exact component={Auth(User, true)}/>
                 <Route path="/user/add" exact component={Auth(AddReview, true)}/>
+                <Route path="/user/register" exact component={Auth(Register, true)}/>
                 <Route path="/user/edit-post/:id" exact component={Auth(EditReview, true)}/>
                 <Route path="/books/:id" exact component={Auth(BookView, null)} />
                 <Route path="/user/user-reviews" exact component={Auth(UserPosts, true)}/>
